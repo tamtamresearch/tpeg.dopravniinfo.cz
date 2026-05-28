eb6393600f593493f844aa4433df4d3bb6562632
rhorenov <roman.horenovsky@gmail.com>
2026-05-28 13:55:05 +0200

    Add floating pre-release banner to every page

--- docs/.vitepress/theme/index.js
+++ docs/.vitepress/theme/index.js
@@ -1,11 +1,20 @@
 import DefaultTheme from "vitepress/theme";
+import { h } from "vue";
 import { useLanguageCookie } from "../composables/useLanguageCookie";
 import "./styles.css";
 
 export default {
     extends: DefaultTheme,
+    Layout() {
+        return h(DefaultTheme.Layout, null, {
+            "layout-bottom": () =>
+                h("div", { class: "prerelease-banner" }, [
+                    h("strong", "Pre-release website. "),
+                    "Content is not final and may change before official launch.",
+                ]),
+        });
+    },
     setup() {
-        // Initialize language cookie tracking
         useLanguageCookie();
     },
 };
